bef07ad3010397c673ece8c9752505afff2f00b6
Maurya <Raj.Maurya@motherson.com>
2025-05-29 09:48:11 +0530

    4

--- src/components/Homepage/firstComponent.tsx
+++ src/components/Homepage/firstComponent.tsx
@@ -1,57 +1,3 @@
-// import React from 'react';
-// import { Button } from 'antd';
-// import landingImage from "../../assets/Homepage/Group 1361498813.png"
-// import motionImage1 from "../../assets/Homepage/Group 1580.png";
-// import motionImage2 from "../../assets/Homepage/Group 5836.png"; // Adjust the path as necessary
-
-// const FirstComponent: React.FC = () => {
-//   return (
-//     <div className=" bg-[#ECF7FB] font-montserrat flex flex-col md:flex-row items-center justify-between bg-white pt-20 px-6 md:px-16">
-//       {/* Left: Text Section */}
-//       <div className="pl-6 md:w-1/2 space-y-6">
-//         <h1 className="font-semibold text-[#183B56]  text-5xl text-gray-800">
-//           We Craft ;<br />
-//           Deploy & Create<br />
-//          Scalable Apps
-//         </h1>
-//         <p className="text-lg text-[#5A7184]">
-//           Build. Launch. Repeat.<br/>
-//           Productive & Creative Tools!<br/>
-//           Make Your Day Life Easy.
-//         </p>
-//         <Button type="primary" size="large">
-//           Explore Apps
-//         </Button>
-//       </div>
-
-//       {/* Right: Image Section */}
-//             <div className="md:w-1/2 mt-8 md:mt-0 relative flex justify-center items-center h-full">
-//         {/* Main landing illustration (base layer) */}
-//         <img
-//           src={landingImage}
-//           alt="Landing Illustration"
-//           className="w-full max-w-md relative z-0"
-//         />
-//         {/* Top decorative image (overlay) */}
-//         <img
-//           src={motionImage1}
-//           alt="Top Decoration"
-//           className="absolute top-16 left-0 w-1/3 max-w-sm z-10 animate-float-slow"
-//         />
-//         {/* Top decorative image (overlay) */}
-//         <img
-//           src={motionImage2}
-//           alt="Top Decoration"
-//           className="absolute top-30 right-28 w-1/6 max-w-sm z-10 animate-float-slow"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FirstComponent;
-
-
 import React from 'react';
 import { Button } from 'antd';
 import landingImage from "../../assets/Homepage/Group 1361498813.png";
@@ -77,7 +23,7 @@ const FirstComponent: React.FC = () => {
           <p className="text-lg text-[#5A7184]">
             Build. Launch. Repeat.<br />
             Productive & Creative Tools!<br />
-            Make Your Day-to-Day Easy.
+            Make Your Day-to-Day Easy. 
           </p>
           <Button type="primary" size="large">
             Explore Apps
